45bd0e13acc61522c2e52ce8283c3335548586c8
Ilona <ilona.rumak7@gmail.com>
2021-10-23 20:04:03 +0300

    task 5-6 modified

--- src/Tasks/Task5.java
+++ src/Tasks/Task5.java
@@ -4,15 +4,14 @@ import java.util.Scanner;
 
 public class Task5 {
     public static void main(String[] args) {
-        String[] dayOfTheWeek = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-        int number;
-        Scanner in = new Scanner(System.in);
+        String[] weekDays = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
         System.out.print("Enter the ordinal number of the day of the week: ");
-        number = in.nextInt();
+        Scanner in = new Scanner(System.in);
+        int number = in.nextInt();
 
         if (number < 7) {
-            System.out.println("The selected day of the week: " + dayOfTheWeek[number]);
+            System.out.println("The selected day of the week: " + weekDays[number]);
         } else {
             System.out.println("Invalid number!");
         }
--- src/Tasks/Task6.java
+++ src/Tasks/Task6.java
@@ -4,33 +4,32 @@ import java.util.Scanner;
 
 public class Task6 {
     public static void main(String[] args) {
-        int number;
-        Scanner in = new Scanner(System.in);
-
         System.out.print("Enter the ordinal number of the day of the week: ");
-        number = in.nextInt();
+        Scanner in = new Scanner(System.in);
+        int number = in.nextInt();
 
+        String selectedDayPhrase = "The selected day of the week: ";
         switch (number) {
             case 0:
-                System.out.println("The selected day of the week: Sunday");
+                System.out.println(selectedDayPhrase + "Sunday");
                 break;
             case 1:
-                System.out.println("The selected day of the week: Monday");
+                System.out.println(selectedDayPhrase + "Monday");
                 break;
             case 2:
-                System.out.println("The selected day of the week: Tuesday");
+                System.out.println(selectedDayPhrase + "Tuesday");
                 break;
             case 3:
-                System.out.println("The selected day of the week: Wednesday");
+                System.out.println(selectedDayPhrase + "Wednesday");
                 break;
             case 4:
-                System.out.println("The selected day of the week: Thursday");
+                System.out.println(selectedDayPhrase + "Thursday");
                 break;
             case 5:
-                System.out.println("The selected day of the week: Friday");
+                System.out.println(selectedDayPhrase + "Friday");
                 break;
             case 6:
-                System.out.println("The selected day of the week: Saturday");
+                System.out.println(selectedDayPhrase + "Saturday");
                 break;
             default:
                 System.out.println("Invalid number!");
